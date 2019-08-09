b2ad02badebbf2d6bb79de5a2d69707715f9bed0
Galdor Takacs <galdor.takacs@rub.de>
2019-08-09 16:53:37 +0200

    counter.service: Add tests

--- src/app/services/counter.service.spec.ts
+++ src/app/services/counter.service.spec.ts
@@ -3,10 +3,42 @@ import { TestBed } from '@angular/core/testing';
 import { CounterService } from './counter.service';
 
 describe('CounterService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let counterService: CounterService;
 
-  it('should be created', () => {
-    const service: CounterService = TestBed.get(CounterService);
-    expect(service).toBeTruthy();
+  function expectCount(expectedCount: number) {
+    let actualCount: number | undefined;
+    counterService.getCount().subscribe((count) => {
+      actualCount = count;
+    });
+    expect(actualCount).toBe(expectedCount);
+  }
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    counterService = TestBed.get(CounterService);
+  });
+
+  it('returns the count', () => {
+    expectCount(0);
+  });
+
+  it('increments the count', () => {
+    counterService.increment();
+    expectCount(1);
+    counterService.increment();
+    expectCount(2);
+  });
+
+  it('decrements the count', () => {
+    counterService.decrement();
+    expectCount(-1);
+    counterService.decrement();
+    expectCount(-2);
+  });
+
+  it('resets the count', () => {
+    const newCount = 1234;
+    counterService.reset(newCount);
+    expectCount(newCount)
   });
 });
